d4e1642942970118eea65b21b16b88166786b47b
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2025-04-05 19:56:41 +0530

    are you ready

--- config/db.js
+++ config/db.js
@@ -6,19 +6,29 @@ dotenv.config();
 const {Pool} = pkg;
 
 
-const  pool = new Pool(
-    {
+// const  pool = new Pool(
+//     {
      
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASS,
-        port: process.env.DB_PORT,
+//         user: process.env.DB_USER,
+//         host: process.env.DB_HOST,
+//         database: process.env.DB_NAME,
+//         password: process.env.DB_PASS,
+//         port: process.env.DB_PORT,
       
       
-    }
-)
+//     }
+// )
 
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, 
+  });
+  
+  pool.query(` id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  roll TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL`);
 
 
 
